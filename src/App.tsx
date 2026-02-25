f71d1f7ab2fc5053f674e1dbed16bcc5d8ca4407
yys7517 <yys7517@naver.com>
2026-02-25 17:43:50 +0900

    feat: Router 설정, Header 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes } from "react-router";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes></Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>윤영선의 포트폴리오</div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <a href="#intro">Home</a>
+          </li>
+          <li>
+            <a href="#skill">Skills</a>
+          </li>
+          <li>
+            <a href="#project">Projects</a>
+          </li>
+          <li>
+            <a href="#career">Careers</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
