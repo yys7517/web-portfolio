46944e6fe5c4a8b357e4c675854840abeb1e1ecb
yys7517 <yys7517@naver.com>
2026-02-27 18:46:03 +0900

    ui: Careeer 섹션 추가

---
+++ src/section/career/CareerSection.tsx
@@ -0,0 +1,11 @@
+import styles from "./CareerSection.module.css";
+
+const CareerSection = () => {
+  return (
+    <section id="career" className={styles.careerSection}>
+      <h2 className={styles.title}>CAREER</h2>
+    </section>
+  );
+};
+
+export default CareerSection;
